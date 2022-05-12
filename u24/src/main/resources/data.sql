052b9a6e111f5e97b64bfa577b5b9dfa900204df
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-05-12 13:03:29 +0200

    Creado data.sql Empleado

---
+++ u24/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+DROP table IF EXISTS empleado;
+
+create table empleado(
+	id int auto_increment,
+	nombre varchar(250),
+	trabajo varchar(250),
+	salario integer
+);
+
+insert into empleado (nombre, trabajo, salario)values('Jose','Trabajo 1',1234);
+insert into empleado (nombre, trabajo, salario)values('Juan','Trabajo 2',1222);
+insert into empleado (nombre, trabajo, salario)values('Pedro','Trabajo 3',1233);
+insert into empleado (nombre, trabajo, salario)values('Jordi','Trabajo 4',1234);
+insert into empleado (nombre, trabajo, salario)values('Jonatan','Trabajo 5',1555);
