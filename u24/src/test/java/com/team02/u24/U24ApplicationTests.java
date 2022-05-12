fe16885f539c8429fc601d3b30cb23188efc6a68
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-05-12 12:33:45 +0200

    Creado pryecto Eclipse Spring Tools 4

---
+++ u24/src/test/java/com/team02/u24/U24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team02.u24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
